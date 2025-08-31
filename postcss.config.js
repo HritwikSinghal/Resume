f1ef6c15419473a72fa12d19af203499d7dd7ddf
Hritwik <hritwiksinghal@protonmail.com>
2025-08-31 17:56:32 +0530

    postcss: misc
    Signed-off-by: Hritwik <hritwiksinghal@protonmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,22 @@
 /* eslint-env node */
+// https://nextjs.org/docs/pages/guides/post-css
 
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
+    // 'postcss-preset-env': {
+    //   // features: {'nesting-rules': false},
+    // },
+
+    //https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
     'postcss-preset-env': {
-      // features: {'nesting-rules': false},
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
     },
     ...(process.env.NODE_ENV === 'production' ? {cssnano: {}} : {}),
   },
