5d49fcdae577dedc94fd8d921226791973143253
Hritwik <hritwiksinghal@protonmail.com>
2025-09-01 14:36:27 +0530

    add meta viewport
    Signed-off-by: Hritwik <hritwiksinghal@protonmail.com>

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -16,6 +16,11 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <meta content={description} name="description" />
         <link href={`${baseDomain}${pathname}`} key="canonical" rel="canonical" />
 
+        {/*This tag tells the browser to set the viewport width equal to the device's screen width and to initialize
+           zoom level at 100%. By doing this, the website can be displayed properly on mobile devices without requiring
+           users to scroll horizontally or zoom in/out.*/}
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
         <link href="/favicon.png" rel="icon" sizes="any" />
         <link href="/favicon.png" rel="icon" sizes="16x16" />
         <link href="/favicon.png" rel="icon" sizes="32x32" />
